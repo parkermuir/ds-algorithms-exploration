8748c736d2b8d0f1d4ed5692bdaeab0a52744700
parkermuir <parker.muir@gmail.com>
2018-10-10 11:50:04 -0400

    bestBuy (easy)

---
+++ bestTimeBuySellStock.js
@@ -0,0 +1,24 @@
+const bestBuy = ( days ) => {
+  let min = days[0];
+  let maxProfit = 0;
+
+  for (let i = 0; i < days.length; i++) {
+    const price = days[i];
+    maxProfit = Math.max(price - min, maxProfit);
+    if ( price < min) {
+      min = price;
+    }
+  }
+  return maxProfit;
+};
+
+
+let a = [ 7, 1, 5, 3, 6, 4];
+let b = [ 7, 8, 5, 4, 6, 12];
+let c = [ 7, 6, 5, 3, 2, 1];
+
+console.log(bestBuy(a)); // 5
+console.log(bestBuy(b)); // 8
+console.log(bestBuy(c)); // 0
+
+// O(n)
