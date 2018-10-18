7d16efe2064b564606a35bb350126e7835e57222
parkermuir <parker.muir@gmail.com>
2018-10-18 19:41:30 -0400

    houseRobber set up

---
+++ houseRobber2.js
@@ -0,0 +1,16 @@
+
+//Houses are in a circle
+//adjacent houses have security system and will contact police
+//determine maximum amount of money without alerting the police
+
+
+let a = [2, 3, 2]; //3
+let b = [1, 2, 3, 4] // 4
+
+const optimalRobber = ( array ) => {
+
+};
+
+
+console.log(optimalRobber(a)); //
+console.log(optimalRobber(b)); //
