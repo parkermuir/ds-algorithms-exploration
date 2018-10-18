ebfab4ff643760d457751fbee9ceef32cf2c8928
parkermuir <parker.muir@gmail.com>
2018-10-17 22:00:06 -0400

    findUncommonCharacters set up

---
+++ findUncommonCharacters.js
@@ -0,0 +1,17 @@
+//give two strings find uncommon characters
+
+
+const uncommonChars = (str1, str2) => {
+  let obj = {};
+  for (char of str1 + str2) {
+    obj[char] = 0;
+  }
+
+  for (char of str1) {
+    
+  }
+
+
+
+
+};
