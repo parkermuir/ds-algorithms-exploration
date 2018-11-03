9fcea36c6080604187a28c7c832e082b504726d7
parkermuir <parker.muir@gmail.com>
2018-11-03 15:05:39 -0400

    dailyCodingProblem version of twosum

--- twoSum.js
+++ twoSum.js
@@ -22,4 +22,27 @@ const twoSum = ( nums, target ) => {
 
 console.log(twoSum(a, 3));
 console.log(twoSum(b, 15));
-console.log(twoSum(c, 8));
+console.log(twoSum(c, 3));
+
+
+/*Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?*/
+
+const booleanTwoSum = (nums, k) => {
+  let history = {};
+  for (let i = 0; i < nums.length; i++) {
+    const num = nums[i];
+    history[num] = null;
+    if (history.hasOwnProperty(k-num)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(booleanTwoSum(a, 3));
+console.log(booleanTwoSum(b, 15));
+console.log(booleanTwoSum(c, 55));
