a38cb25974629a37a04ce653b901d7eadf9f22a5
parkermuir <parker.muir@gmail.com>
2018-11-09 23:21:47 -0500

    rate limiter with arguments working

--- recentCalls.js
+++ recentCalls.js
@@ -15,21 +15,22 @@ const recentCounter = {
   },
 };
 
-const instance = Object.create(recentCounter);
+// const instance = Object.create(recentCounter);
 
-console.log(instance.ping(1));
-console.log(instance.ping(100));
-console.log(instance.ping(3001));
-console.log(instance.ping(3002));
+// console.log(instance.ping(1));
+// console.log(instance.ping(100));
+// console.log(instance.ping(3001));
+// console.log(instance.ping(3002));
 
 const rateLimiter = (func, n) => {
   calls = [];
+
   return function () {
-    let time = new Date();
-    calls.push(time);
-    calls = this.calls.filter(call => call > (time - 3000));
-    if (calls.length < n) {
-      func(...arguments);
+    let timestamp = new Date();
+    calls.push(timestamp);
+    calls = calls.filter(call => call > (timestamp - 1000));
+    if (calls.length <= n) {
+      func.apply(null, arguments);
     } else {
       return;
     }
@@ -37,8 +38,9 @@ const rateLimiter = (func, n) => {
 };
 
 
-let caller = rateLimiter( ()=> { 
-  console.log(...arguments);
+let caller = rateLimiter( function () {
+  let args = [...arguments];
+  console.log(args);
 }, 3);
 
 caller('hi', 'hey');
