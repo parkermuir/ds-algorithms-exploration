32d89cb0cb00176e89ec55f679edd89c87c1097d
parkermuir <parker.muir@gmail.com>
2018-11-09 20:20:42 -0500

    rateLimiter attempt

--- recentCalls.js
+++ recentCalls.js
@@ -21,3 +21,28 @@ console.log(instance.ping(1));
 console.log(instance.ping(100));
 console.log(instance.ping(3001));
 console.log(instance.ping(3002));
+
+const rateLimiter = (func, n) => {
+  calls = [];
+  return function () {
+    let time = new Date();
+    calls.push(time);
+    calls = this.calls.filter(call => call > (time - 3000));
+    if (calls.length < n) {
+      func(...arguments);
+    } else {
+      return;
+    }
+  };
+};
+
+
+let caller = rateLimiter( ()=> { 
+  console.log(...arguments);
+}, 3);
+
+caller('hi', 'hey');
+caller('hi', 'hey');
+caller('hi', 'hey');
+caller('hi', 'hey');
+caller('hi', 'hey');
