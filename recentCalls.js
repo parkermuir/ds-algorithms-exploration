cb122d0b7882a87372620a8816f8aac43446f962
parkermuir <parker.muir@gmail.com>
2018-11-09 19:48:55 -0500

    recent Calls class implementation

--- recentCalls.js
+++ recentCalls.js
@@ -1,5 +1,23 @@
 //933
 
-const recentCounter = () => {
-  
+/** 
+ * Your RecentCounter object will be instantiated and called as such:
+ * var obj = Object.create(RecentCounter).createNew()
+ * var param_1 = obj.ping(t)
+ */
+
+const recentCounter = {
+  calls: [],
+  ping: function (t) {
+    this.calls.push(t);
+    this.calls = this.calls.filter( call => call >= (t - 3000));
+    return this.calls.length;
+  },
 };
+
+const instance = Object.create(recentCounter);
+
+console.log(instance.ping(1));
+console.log(instance.ping(100));
+console.log(instance.ping(3001));
+console.log(instance.ping(3002));
