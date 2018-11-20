7ec1b843a899ca11634431fa7102daef4fa89e08
parkermuir <parker.muir@gmail.com>
2018-11-20 16:50:04 -0500

    remove nodes from LL - leet accept

--- removeLLElements.js
+++ removeLLElements.js
@@ -1,25 +1,21 @@
 var removeElements = function (head, val) {
-
-  if (!head) {
-    return null;
-  }
-
-  if (head.val === val) {
-    head = head.next;
-  }
-
-  let prev = head;
-  let current = head.next;
+  let prev = null;
+  let current = head;
 
   while (current !== null) {
+
     if (current.val === val) {
-      let temp = current;
-      prev.next = current.next;
-      current = temp.next;
+      if (prev === null) {
+        head = current.next;
+      } else {
+        prev.next = current.next;
+      }
+
     } else {
-      current = current.next;
-      prev = prev.next;
+      prev = current;
     }
+
+    current = current.next;
   }
   return head;
 };
