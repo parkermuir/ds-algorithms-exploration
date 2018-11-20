285ff62ca30cd312e378e3e8a37b5309eb909135
parkermuir <parker.muir@gmail.com>
2018-11-20 16:33:23 -0500

    remove node from LL

---
+++ removeLLElements.js
@@ -0,0 +1,21 @@
+var removeElements = function(head, val) {
+    
+  if (head.val === val) {
+      head = head.next
+  }
+  
+  let prev = head;
+  let current = head.next;
+  
+  while ( current !== null ) {
+      if (current.val === val) {
+        let temp = current
+        prev.next = current.next
+        current = temp.next
+      } else {
+        current = current.next
+        prev = prev.next
+      }
+  }
+  return head;
+};
