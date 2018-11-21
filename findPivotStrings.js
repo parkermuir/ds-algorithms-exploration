2ea834c8478e6e8996a5e3e66016c4c08820f05e
parkermuir <parker.muir@gmail.com>
2018-11-21 18:48:29 -0500

    pivot array of strings

---
+++ findPivotStrings.js
@@ -0,0 +1,13 @@
+
+let a = ['p', 'q', 'z', 'a', 'b', 'c', 'd', 'e', 'f'];
+const findPivot = (arr) => {
+  let firstWord = arr[0];
+  let front = 0;
+  let back = array.length - 1;
+  let mid;
+
+  while ( front < back) {
+    
+  }
+
+};
