4f450bc6547453b2f7e68821a1f2c2bbf29bab4f
parkermuir <parker.muir@gmail.com>
2018-12-03 20:24:14 -0500

    remove nth from the end

---
+++ removeNthFromEndLL.js
@@ -0,0 +1,20 @@
+
+const removeNthFromEnd = function (head, n) {
+  let current = head;
+  let leader;
+  let count;
+
+  while (current2 !== null) {
+    while (count < n) {
+      leader = leader.next;
+      count++;
+    }
+
+    current = current.next;
+    leader = leader.next;
+
+  }
+
+  //once this loop breaks you need remove the node
+
+};
