6d4f711a6057384885f26d8880ba1ff79ba78b6d
parkermuir <parker.muir@gmail.com>
2018-12-07 21:24:12 -0500

    clean up removeNthFromEnd

--- removeNthFromEndLL.js
+++ removeNthFromEndLL.js
@@ -3,44 +3,46 @@ const Node = function(value) {
   this.next = null;
 };
 
-let head = new Node(0);
-let current = head;
-let n = 1;
-while (n < 11) {
-  current.next = new Node(n++);
-  current = current.next;
-}
+const createList = (length) => {
+  let head = new Node(0);
+  let current = head;
+  let count = 1;
 
+  while (count < length) {
+    current.next = new Node(count++);
+    current = current.next;
+  }
+
+  return head;
+};
 
 const removeNthFromEnd = function (head, n) {
   let current = head;
   let leader = head;
   let count = 0;
 
-  //
+  //move leader to starting point n + 1 nodes ahead of current
+  //n + 1 because current wants to end up one before the target prior to removal
   while (count <= n) {
     leader = leader.next;
     count++;
   }
 
-  console.log('Count=', count);
-  console.log('leader', leader);
+  // console.log('Count=', count);
+  // console.log('leader', leader);
   
   while (leader !== null) {
     current = current.next;
     leader = leader.next;
   }
   
-  current.next = current.next.next;
-  // current.next = temp;
+  let temp = current.next.next;
+  current.next = temp;
 
-  console.log('leader', leader);
-  console.log('current', current);
+  // console.log('leader', leader);
+  // cogit adgnsole.log('current', current);
 
-  //once this loop breaks you need remove the node
-  //then return head
-
+  return head;
 };
 
-
-removeNthFromEnd(head, 3);
+console.log(removeNthFromEnd(createList(1), 1));
