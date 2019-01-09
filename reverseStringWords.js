8421c45e43e2814ae470d32d0509b66f6ce3a6f4
parkermuir <parker.muir@gmail.com>
2019-01-08 23:55:08 -0500

    univaluedBT

--- reverseStringWords.js
+++ reverseStringWords.js
@@ -3,7 +3,18 @@
  * @returns {string}
  */
 
- 
-const reverseWords = function(str) {
-    
+let a = ' the    sky  is blue  ';
+
+const reverseWords = (str) => {
+  let words = str.split(' ');
+  let reversed = '';
+  for (let i = words.length - 1; i >= 0; i--) {
+    if (words[i] !== '') {
+      reversed = reversed + words[i] + ' ';
+    }
+  }
+
+  return reversed.slice(0, reversed.length - 1);
 };
+
+console.log(reverseWords(a));
