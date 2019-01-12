52d079002f7d0f5e8091bbae5cb5c0d33ef8e71f
parkermuir <parker.muir@gmail.com>
2019-01-11 21:31:57 -0500

    largestInEachBTreeRow solution

--- largestInEachBTreeRow.js
+++ largestInEachBTreeRow.js
@@ -12,5 +12,17 @@
  * @return {number[]}
  */
 var largestValues = function(root) {
-
+  let results = [];
+  
+  const dfs = (node, depth) => {
+    if (!node) return;
+    if (results[depth] === undefined) results[depth] = node.val;
+    else results[depth] = Math.max(results[depth], node.val);
+    
+    dfs(node.left, depth + 1);
+    dfs(node.right, depth + 1);
+  }
+  
+  dfs(root, 0);
+  return results;
 };
