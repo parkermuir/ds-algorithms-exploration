9962996a8f0c5fe28456e854c484a27dba63d163
parkermuir <parker.muir@gmail.com>
2019-01-15 15:34:02 -0500

    nestedULOL

---
+++ nestedULOL.js
@@ -0,0 +1,21 @@
+/*
+
+You are given a DOM tree and have to analyze the <ul> and <ol> list tags within it. Your task is to find the maximum depth of nested <ul>/<ol> list tags. A single <ul>/<ol> list is nested one level deep. Each <ul>/<ol> list inside another <ul>/<ol> list is nested one level deeper. If there are no <ul> or <ol> lists at all in the DOM tree, the depth of nesting is 0.
+
+Note that <ul>/<ol> lists can be nested directly or indirectly; that is, a <ul> list inside a table inside an <ol> list is nested two levels deep.
+
+
+Write a function:
+
+  function solution();
+that, given a DOM tree, returns the maximum depth of nested <ul>/<ol> lists.
+
+Assume that:
+
+the DOM tree represents a valid HTML5 document;
+length of the HTML document does not exceed 4KB;
+jQuery 2.1 is supported.
+
+*/
+
+
