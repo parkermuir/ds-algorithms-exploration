4ceee48bf027eded3a0f94cec56021289703e1d2
parkermuir <parker.muir@gmail.com>
2019-01-18 11:27:26 -0500

    longestSubstringWithoutReapeat solution

--- longestSubstringWithoutRepeat.js
+++ longestSubstringWithoutRepeat.js
@@ -4,5 +4,34 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-}:
+var lengthOfLongestSubstring = function (s) {
+  let beginning = 0
+  let end = 1
+  let maxLen = 0
+
+  while (end < s.length) {
+    if (isUnique(s, beginning, end)) {
+      console.log(end - beginning)
+      if ((end - beginning) > maxLen) {
+        maxLen = end - beginning
+      }
+      end++
+    } else {
+      beginning++
+    }
+  }
+
+  return maxLen
+};
+
+const isUnique = (string, start, stop) => {
+  let seen = {}
+  for (let i = start; i < stop; i++) {
+    if (!!seen[string[i]]) {
+      return false
+    } else {
+      seen[string[i]] = 1
+    }
+  }
+  return true
+}
