1ec8d373796339b1d31de1de187b48e8d2b3c192
parkermuir <parker.muir@gmail.com>
2019-01-18 19:24:07 -0500

    rightSideView.js

---
+++ btRightSideView.js
@@ -0,0 +1,20 @@
+//#199
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    let view = [];
+    let q = [root];
+    while (q.length > 0) {
+      
+    }
+};
