dfd69d2fa39e7ad13a1e8c57d2868e379cd77fa3
parkermuir <parker.muir@gmail.com>
2019-01-21 19:14:47 -0500

    fibonacci solution in linear with DP

--- nthFibonacci.js
+++ nthFibonacci.js
@@ -1,7 +1,31 @@
 var fib = function (N) {
+  if (N === 0) {
+    return 0;
+  }
+
   if (N === 1 || N === 2) {
     return 1;
   }
 
   return fib(N - 1) + fib(N - 2);
 };
+
+const fibDP = function(N) {
+  if (N === 0) {
+    return 0;
+  }
+
+  let prev1 = 1;
+  let prev2 = 1;
+  let sum = 0;
+
+  for (let i = 2; i < N; i++) {
+    sum = prev1 + prev2;
+    prev1 = prev2;
+    prev2 = sum;
+  }
+
+  return sum;
+};
+
+console.log(fibDP(6));
