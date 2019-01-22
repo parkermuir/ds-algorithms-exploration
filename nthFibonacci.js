db273c4ec9e7a3ba05d59bab22ff145f620968c1
parkermuir <parker.muir@gmail.com>
2019-01-21 19:07:47 -0500

    formatting

--- nthFibonacci.js
+++ nthFibonacci.js
@@ -1,7 +1,7 @@
-var fib = function(N) {
+var fib = function (N) {
   if (N === 1 || N === 2) {
-      return 1
+    return 1;
   }
-  
-  return fib(N - 1) + fib(N - 2)
+
+  return fib(N - 1) + fib(N - 2);
 };
