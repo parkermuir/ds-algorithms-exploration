178c85468a6b4c8fd7b3a6170e39661a7074d391
parkermuir <parker.muir@gmail.com>
2019-02-08 16:45:46 -0500

    coinsums recursive edits

--- coinSums.js
+++ coinSums.js
@@ -1,9 +1,7 @@
-
-
 const coinSums = (coins, sum) => {
 
-  const countWays = (coins, amount, num) => {
-    if (amount = 0) {
+  const countWays = (coins, amount, num, callStack) => {
+    if (amount === 0) {
       return 1;
     }
 
@@ -15,10 +13,11 @@ const coinSums = (coins, sum) => {
       return 0;
     }
 
+    //two options a) use the coin, decrement the amount b) don't use the coin decrement the num
     return countWays(coins, amount - coins[num], num) + countWays(coins, amount, num - 1);
   };
 
-  countWays(coins, sum, coins.length - 1);
+  return countWays(coins, sum, coins.length - 1);
 };
 
 console.log(coinSums([1, 2, 5], 9));
