c97b39ab3584daba923fbb30242377f2d745fcb8
parkermuir <parker.muir@gmail.com>
2019-02-09 17:46:29 -0500

    mergeIntervals solution code

--- mergeIntervals.js
+++ mergeIntervals.js
@@ -9,6 +9,25 @@
  * @param {Interval[]} intervals
  * @return {Interval[]}
  */
-var merge = function(intervals) {
+var merge = function (intervals) {
+  intervals.sort((a, b) => {
+    return a.start - b.start;
+  })
 
+  let results = [];
+  for (let i = 0; i < intervals.length; i++) {
+    if (i === 0) {
+      results.push(intervals[i])
+    }
+    let curr = intervals[i];
+    let prev = results.pop();
+
+    if (prev.end >= curr.start) {
+      let temp = new Interval(prev.start, Math.max(prev.end, curr.end))
+      results.push(temp);
+    } else {
+      results.push(prev, curr);
+    }
+  }
+  return results;
 };
