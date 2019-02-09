f0c18e2975c65ede85872a150b573e089db2b8ec
parkermuir <parker.muir@gmail.com>
2019-02-09 18:18:46 -0500

    simplifyPath stack solution

--- simplifyPath.js
+++ simplifyPath.js
@@ -4,6 +4,19 @@
  * @param {string} path
  * @return {string}
  */
-var simplifyPath = function(path) {
-    
+var simplifyPath = function (path) {
+  let dirs = path.split('/').filter((dir) => dir.length > 0)
+
+  let stack = [];
+  dirs.forEach((dir) => {
+    if (dir === '.') {
+      return
+    } else if (dir === '..') {
+      stack.pop();
+    } else {
+      stack.push(dir)
+    }
+  })
+
+  return '/' + stack.join('/');
 };
