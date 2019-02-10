0c51a187b8bef179bc0fc5ec64a664175ae93d20
parkermuir <parker.muir@gmail.com>
2019-02-10 18:07:03 -0500

    climbstairs dynamic programming

--- climbStairs.js
+++ climbStairs.js
@@ -1,21 +1,33 @@
 // #70
 
+// const climbStairs = (n) => {
+//   let count = 0;
+
+//   const climb = (stairs) => {
+//     if (stairs === n) {
+//       count++
+//       return;
+//     }
+//     if (stairs > n) {
+//       return;
+//     }
+
+//     climb(stairs + 1)
+//     climb(stairs + 2)
+//   }
+
+//   climb(0)
+//   return count;
+// };
+
 const climbStairs = (n) => {
-  let count = 0;
+  let results = new Array(n + 1);
+  results[0] = 1;
+  results[1] = 1;
 
-  const climb = (stairs) => {
-    if (stairs === n) {
-      count++
-      return;
-    }
-    if (stairs > n) {
-      return;
-    }
-
-    climb(stairs + 1)
-    climb(stairs + 2)
+  for (let i = 2; i < results.length; i++) {
+    results[i] = results[i - 2] + results[i - 1];
   }
 
-  climb(0)
-  return count;
+  return results[results.length - 1];
 };
