b3be68be78ecc33cac248e0df92c804f8365ba2d
parkermuir <parker.muir@gmail.com>
2019-02-10 18:02:33 -0500

    climb stairs recursive

--- climbStairs.js
+++ climbStairs.js
@@ -1,5 +1,21 @@
 // #70
 
 const climbStairs = (n) => {
+  let count = 0;
 
+  const climb = (stairs) => {
+    if (stairs === n) {
+      count++
+      return;
+    }
+    if (stairs > n) {
+      return;
+    }
+
+    climb(stairs + 1)
+    climb(stairs + 2)
+  }
+
+  climb(0)
+  return count;
 };
