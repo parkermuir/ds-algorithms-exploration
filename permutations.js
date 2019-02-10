435ef45d6ae19dc74d25cf6ae793a39cc149611a
parkermuir <parker.muir@gmail.com>
2019-02-10 16:20:01 -0500

    permutations build ground up solution

--- permutations.js
+++ permutations.js
@@ -2,6 +2,25 @@
 // #46
 
 
-var permute = function(nums) {
-    
+var permute = function (nums) {
+  let results = [];
+
+  const generatePermutations = (current) => {
+    if (current.length === nums.length) {
+      return results.push(current);
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+      //if current element is not in the current permutation being built, concat it and recurse
+      if (current.indexOf(nums[i]) === -1) {
+        generatePermutations(current.concat(nums[i]));
+      }
+    }
+
+  };
+
+  generatePermutations([]);
+  return results;
 };
+
+console.log(permute(['a', 'b', 'c']));
