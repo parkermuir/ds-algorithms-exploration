e985a0da4bed4d7732fc5557f778eaa96a45ecbe
parkermuir <parker.muir@gmail.com>
2019-02-09 21:04:38 -0500

    decodeString stack solution

--- decodeString.js
+++ decodeString.js
@@ -4,6 +4,42 @@
  * @param {string} s
  * @return {string}
  */
-var decodeString = function(s) {
+var decodeString = function (s) {
+  let strings = [''];
+  let nums = [0]
 
+  for (let i = 0; i < s.length; i++) {
+    let char = s[i];
+    if (char === '[') {
+      strings.push('')
+    } else if (char === ']') {
+      let temp = strings.pop()
+      let num = nums.pop()
+      let newStr = multiplier(temp, num);
+      let prev = strings.pop();
+      strings.push(prev.concat(newStr));
+    } else if (isNaN(char)) {
+      temp = strings.pop()
+      newStr = temp.concat(char)
+      strings.push(newStr)
+    } else if (!isNaN(char)) {
+      if (!isNaN(s[i - 1])) {
+        let tempNum = nums.pop()
+        tempNum *= 10;
+        tempNum += parseInt(char);
+        nums.push(tempNum);
+      } else {
+        nums.push(parseInt(char));
+      }
+    }
+  }
+  return strings[0];
+};
+
+const multiplier = (string, num) => {
+  let results = ''
+  for (let i = 0; i < num; i++) {
+    results = results + string
+  }
+  return results;
 };
