c857d008770e3ce2eeb637144a11abf12dffb086
parkermuir <parker.muir@gmail.com>
2019-02-10 16:50:17 -0500

    bottom up solution for string permutations

--- permutations.js
+++ permutations.js
@@ -23,4 +23,33 @@ var permute = function (nums) {
   return results;
 };
 
-console.log(permute(['a', 'b', 'c']));
+// console.log(permute(['a', 'b', 'c']));
+
+const permuteString = (string) => {
+  //base case
+  if (string.length <= 1) {
+    return [string];
+  }
+
+  let remainingChars = string.slice(0, -1);
+  let lastChar = string[string.length - 1];
+  console.log({remainingChars, lastChar});
+
+  //recurse to get all possible permutations of the reaminingChars string
+  let tempPermutations = permuteString(remainingChars);
+
+
+  const results = [];
+
+  tempPermutations.forEach( (perm) => {
+    for (let i = 0; i <= remainingChars.length; i++) {
+      let temp = perm.slice(0, i) + lastChar + perm.slice(i);
+      results.push(temp);
+    }
+  });
+
+
+  return results;
+};
+
+console.log(permuteString('abce'));
