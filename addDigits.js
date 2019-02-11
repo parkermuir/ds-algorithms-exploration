85e64b4e3326a682eb073117f29efc1f047d4bbc
parkermuir <parker.muir@gmail.com>
2019-02-10 20:21:52 -0500

    addDigits solution recursive

--- addDigits.js
+++ addDigits.js
@@ -1,5 +1,17 @@
 // #258
 
 const addDigits = (num) => {
-
+  num = num.toString();
+    
+  if (num.length === 1) {
+    return parseInt(num);
+  }
+  
+  let newNum = 0;
+  
+  for (let i = 0; i < num.length; i++ ) {
+    newNum += parseInt(num[i]);
+  }
+  
+  return addDigits(newNum);
 };
