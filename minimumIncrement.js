6b958bc9f4d21e590e158f4608df5722765eec32
parkermuir <parker.muir@gmail.com>
2019-02-17 18:14:00 -0500

    minimumIncrementUnique solution

--- minimumIncrement.js
+++ minimumIncrement.js
@@ -3,6 +3,35 @@
  * @param {number[]} A
  * @return {number}
  */
-var minIncrementForUnique = function(A) {
+var minIncrementForUnique = function (A) {
+  let hash = createHash(A);
+  let moves = 0;
 
+  for (let i = 0; i < A.length; i++) {
+    if (hash[A[i]] === 1) {
+      continue;
+    } else if (hash[A[i]] > 1) {
+      hash[A[i]]--
+      while (hash.hasOwnProperty(A[i])) {
+        A[i]++
+        moves++
+      }
+      hash[A[i]] = 1;
+    }
+  }
+  return moves;
+};
+
+
+var createHash = function (A) {
+  let result = {}
+  for (let i = 0; i < A.length; i++) {
+    let curr = A[i]
+    if (curr in result) {
+      result[curr]++
+    } else {
+      result[curr] = 1
+    }
+  }
+  return result;
 }
