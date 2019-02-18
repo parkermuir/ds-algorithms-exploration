baaa7cb76c03eedbc4cab747f7aa6d004a5c30bf
parkermuir <parker.muir@gmail.com>
2019-02-18 17:41:11 -0500

    jumpGameII solution

--- jumpGameII.js
+++ jumpGameII.js
@@ -4,6 +4,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-var jump = function(nums) {
+var jump = function (nums) {
+  let dp = new Array(nums.length).fill(0);
 
+  for (let i = nums.length - 2; i >= 0; i--) {
+    dp[i] = calculateMin(dp, i + 1, i + nums[i]) + 1;
+  }
+
+  return dp[0];
+};
+
+const calculateMin = (array, left, right) => {
+  let min = Infinity;
+  for (let i = left; i <= right && i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
 };
