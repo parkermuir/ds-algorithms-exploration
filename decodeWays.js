699e98464f1717c49eed8d4e9b162230db26a1f1
parkermuir <parker.muir@gmail.com>
2019-02-22 16:47:09 -0500

    decodeWays pseudocode

---
+++ decodeWays.js
@@ -0,0 +1,16 @@
+// #91
+
+/*
+ * @param {string} s
+ * @return {number}
+ */
+var numDecodings = function(s) {
+  //only 10 to 26 are valid ways of decoding 2 digits
+  
+  //recurse with remaining string window
+  
+  //two paths
+      //recurse with single digit
+      //recurse with two digits
+  
+};
