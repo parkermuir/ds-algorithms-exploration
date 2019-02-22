95b9695c9f55c5709f6057574baf17a14be7d1ca
parkermuir <parker.muir@gmail.com>
2019-02-22 12:37:39 -0500

    firstRepeatingChar solution

--- firstRepeatingChar.js
+++ firstRepeatingChar.js
@@ -4,7 +4,29 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function(s) {  
+var firstUniqChar = function (s) {
+  let hash = {}
 
-  
+  for (var i = 0; i < s.length; i++) {
+    if (!hash.hasOwnProperty(s[i])) {
+      hash[s[i]] = i
+    } else {
+      hash[s[i]] = 'repeat'
+    }
+  }
+
+  let min = Infinity
+
+  for (key in hash) {
+    if (typeof hash[key] === 'number' && hash[key] < min) {
+      min = hash[key]
+    }
+  }
+
+  if (min === Infinity) {
+    return -1
+  } else {
+    return min
+  }
+
 };
