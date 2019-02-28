a38581ab4a6a87f09310b8171375cba230c9b8bc
parkermuir <parker.muir@gmail.com>
2019-02-28 11:10:48 -0500

    post order traversal iterative solution

--- postOrderTraversalBT.js
+++ postOrderTraversalBT.js
@@ -11,5 +11,22 @@
  */
 
 var postorderTraversal = function (root) {
+  let stack = [root];
+  let stack2 = [];
+  let results = [];
 
+  while (stack.length > 0) {
+    node = stack.pop();
+    stack2.push(node.val);
+    if (node.left) { stack.push(node.left); }
+    if (node.right) { stack.push(node.right); }
+  };
+
+  while (stack2.length > 0) {
+    let val = stack2.pop();
+    results.push(val);
+  }
+
+  return results;
+  //return results.reverse(); after pushing into results instead of stack2
 };
