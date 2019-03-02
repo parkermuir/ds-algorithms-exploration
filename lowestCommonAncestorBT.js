7541c5f8ab2c8f8c4f38c3ba2677c6e8aedb1447
parkermuir <parker.muir@gmail.com>
2019-03-01 20:49:49 -0500

    lowest common ancestor in BT recursive solution with count

---
+++ lowestCommonAncestorBT.js
@@ -0,0 +1,24 @@
+var lowestCommonAncestor = function (root, p, q) {
+  let result = null
+
+  const dfs = (node) => {
+    if (!node) return 0;
+
+    let count = 0;
+
+    if (node.val === p.val || node.val === q.val) {
+      count = 1
+    }
+
+    count += dfs(node.left)
+    count += dfs(node.right);
+
+    if (count === 2 && !result) {
+      result = node;
+    }
+
+    return count;
+  }
+  dfs(root)
+  return result;
+};
