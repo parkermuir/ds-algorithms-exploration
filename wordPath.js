3e5f74322a333228a3762ccadcd658643c2900b0
parkermuir <parker.muir@gmail.com>
2019-03-04 20:30:51 -0500

    wordPath BFS solution without preprocessing

--- wordPath.js
+++ wordPath.js
@@ -1,20 +1,49 @@
-while (q.length > 0) {
-  let size = q.length;
-  
-  for (let w = 0; w < size; w++) {
+//#127
+
+
+/*
+ * @param {string} beginWord
+ * @param {string} endWord
+ * @param {string[]} wordList
+ * @return {number}
+ */
+var ladderLength = function (beginWord, endWord, words) {
+  let visited = {};
+  visited[beginWord] = true;
+  let q = [beginWord];
+  let count = 1;
+
+  while (q.length > 0) {
+    let size = q.length;
+
+    for (let w = 0; w < size; w++) {
       let word = q.shift();
       for (let i = 0; i < words.length; i++) {
-          if (word === words[i]) {
-              return true;
-          }
-      }
-      
-      if (!visited.hasOwnProperty(words[i])) {
+        if (word === endWord) {
+          return count;
+        }
+
+        if (!visited.hasOwnProperty(words[i])) {
           if (oneEdit(word, words[i])) {
-              q.push(words[i]) 
-              visited[words[i]] = true;
-          }    
-      }  
+            q.push(words[i])
+            visited[words[i]] = true;
+          }
+        }
+      }
     }
-  }      
+    count++
+  }
+
+  return 0;
+};
+
+const oneEdit = (word1, word2) => {
+  let diffs = 0;
+  for (let i = 0; i < word1.length; i++) {
+    if (word1[i] !== word2[i]) {
+      diffs++
+    }
+  }
+
+  return diffs === 1;
 }
