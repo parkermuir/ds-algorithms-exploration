14baf132954e744253493cc2c484b70c0f54f288
parkermuir <parker.muir@gmail.com>
2019-03-09 18:47:26 -0500

    fruit into baskets solution

--- fruitIntoBaskets.js
+++ fruitIntoBaskets.js
@@ -5,5 +5,42 @@
  * @return {number}
  */
 var totalFruit = function (array) {
+  let start = 0;
+  let i = 0;
 
+  let baskA = 0;
+  let fruitA = null;
+  let baskB = 0;
+  let fruitB = null;
+
+  let max = 0;
+
+  while (i < array.length) {
+    let fruit = array[i];
+
+    if (fruit !== fruitA && fruit !== fruitB) {
+      while (baskA !== 0 && baskB !== 0) {
+        if (array[start] === fruitA) { baskA-- }
+        if (array[start] === fruitB) { baskB-- }
+        start++
+      }
+    } else {
+      if (fruit === fruitA) { baskA++ }
+      if (fruit === fruitB) { baskB++ }
+    }
+
+    if (baskA === 0 && fruit !== fruitB) {
+      fruitA = fruit;
+      baskA = 1;
+    } else if (baskB === 0 && fruit !== fruitA) {
+      fruitB = fruit;
+      baskB = 1;
+    }
+
+    //console.log({fruitA, baskA, fruitB, baskB})
+    max = Math.max(max, baskA + baskB)
+    i++;
+  }
+
+  return max;
 };
