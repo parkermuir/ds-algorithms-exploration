1f69d3b13412820752b065625b41f2973a2d4621
parkermuir <parker.muir@gmail.com>
2019-03-09 12:02:22 -0500

    sortColors three pointer solution

---
+++ sortColors.js
@@ -0,0 +1,25 @@
+//#75
+
+/*
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+const sortColors = (nums) => {
+  let left = 0;
+  let right = nums.length - 1;
+  let i = 0;
+
+  while (i <= right) {
+
+    if (nums[i] === 0) {
+      [nums[left], nums[i]] = [nums[i], nums[left]]
+      left++
+      i++
+
+    } else if (nums[i] === 2) {
+      [nums[right], nums[i]] = [nums[i], nums[right]]
+      right--
+    } else if (nums[i] === 1)
+      i++
+  }
+};
