0f5343f20362fc23ba8ef3c099df5f099cef1355
parkermuir <parker.muir@gmail.com>
2019-03-10 17:41:03 -0400

    find anagrams in string linear sliding window solution

--- findAnagramsInString.js
+++ findAnagramsInString.js
@@ -62,34 +62,34 @@
 //   return results;
 // }
 
-var findAnagrams = function(s, p) {
+var findAnagrams = function (s, p) {
   let begin = 0;
   let end = 0;
-  let chars = createHash(p)
+  let chars = createHash(p);
   let count = Object.keys(chars).length;
   let results = [];
-  
+
   while (end < s.length) {
-      if (chars.hasOwnProperty(s[end])) {
-          
-          chars[s[end]]--
-          if (chars[s[end]] === 0) { count--; }
+    if (chars.hasOwnProperty(s[end])) {
+
+      chars[s[end]]--
+      if (chars[s[end]] === 0) { count--; }
+    }
+
+    end++
+
+    while (count === 0) {
+      if (end - begin === p.length) {
+        results.push(begin);
       }
-      
-      end++
-      
-      while (count === 0) {
-          if (end - begin === p.length) {
-              results.push(begin);
-          }
-          
-          if (chars.hasOwnProperty(s[end])) {
-  
-              chars[s[begin]]++
-              if (chars[s[begin]] > 0) { count++; }
-          }
-          begin++
+
+      if (chars.hasOwnProperty(s[begin])) {
+
+        chars[s[begin]]++
+        if (chars[s[begin]] > 0) { count++; }
       }
+      begin++
+    }
   }
   return results;
 }
@@ -97,7 +97,9 @@ var findAnagrams = function(s, p) {
 const createHash = (string) => {
   let result = {};
   for (let i = 0; i < string.length; i++) {
-      result[string[i]] = result[string[i]] + 1 || 1
+    result[string[i]] = result[string[i]] + 1 || 1
   }
   return result;
 }
+
+console.log(findAnagrams('cbaebabacd', 'abc'));
