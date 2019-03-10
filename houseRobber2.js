02688077145d82f80292b27fd7c4feb7323a1fce
parkermuir <parker.muir@gmail.com>
2019-03-09 21:32:34 -0500

    house robber 2 pseudocode

--- houseRobber2.js
+++ houseRobber2.js
@@ -8,7 +8,11 @@ let a = [2, 3, 2]; //3
 let b = [1, 2, 3, 4]; // 4
 
 const optimalRobber = ( array ) => {
+  //house robber one but conditional
+  //if selecting the first house
+  //or not selecting the first house
 
+  //compare the output of the two
 };
 
 
