a32b8c6fffe6fddcc7ecd823022cde38c773a2b7
parkermuir <parker.muir@gmail.com>
2019-03-10 14:14:48 -0400

    min window substr working logic, needs edge help

--- minimumWindowSubstring.js
+++ minimumWindowSubstring.js
@@ -4,6 +4,46 @@
  * @param {string} t
  * @return {string}
  */
-var minWindow = function(s, t) {
-    
+var minWindow = function (s, t) {
+  let min = s;
+  let left = 0;
+  let right = 0;
+  let target = makeFreqHash(t)
+  let current = {};
+
+  while (right < s.length) {
+    if (target.hasOwnProperty(s[right])) {
+      current[s[right]] = current[s[right]] + 1 || 1
+    }
+
+    while (isValid(target, current)) {
+      if (right - left + 1 < min.length) {
+        min = s.substring(left, right + 1);
+      }
+      if (current.hasOwnProperty(s[left])) {
+        current[s[left]]--
+      }
+      left++
+    }
+    right++
+  }
+
+  return min;
 };
+
+const makeFreqHash = (string) => {
+  let result = {};
+  for (let i = 0; i < string.length; i++) {
+    result[string[i]] = result[string[i]] + 1 || 1;
+  }
+  return result;
+}
+
+const isValid = (target, current) => {
+  for (key in target) {
+    if (!current[key] || current[key] < target[key]) {
+      return false;
+    }
+  }
+  return true;
+}
