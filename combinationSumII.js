b7ba13b91eee423e01faf1d118bd03caf5907715
parkermuir <parker.muir@gmail.com>
2019-03-10 18:26:16 -0400

    combination sum II solution

--- combinationSumII.js
+++ combinationSumII.js
@@ -1,20 +1,30 @@
-var combinationSum2 = function (candidates, target) {
+//# 40
+var combinationSum2 = function(candidates, target) {
   let results = [];
-
-  let createCombinations = (currentCombo, sum) => {
-    if (sum === target) {
-      results.push(currentCombo);
-      return;
-    }
-
-    for (let i = 0; i < candidates.length; i++) {
-      let num = candidates[i];
-      if (!currentCombo.includes(num) && num + sum <= target) {
-        createCombinations(currentCombo.concat(num), sum + num);
+  
+  candidates = candidates.sort((a, b) => {
+      return a - b;
+  })
+  
+  console.log(candidates)
+  
+  let createCombinations = (currentCombo, sum, start) => {
+      if (sum === target) {
+          results.push(currentCombo);
+          return;
       }
-    }
+      
+      for (let i = start; i < candidates.length; i++) {
+          let num = candidates[i];
+          
+          if (i !== start && num === candidates[i - 1]) { continue; }
+          
+          if (num + sum <= target) {
+              createCombinations(currentCombo.concat(num), sum + num, i + 1);
+          }  
+      }
   }
-
-  createCombinations([], 0)
+  
+  createCombinations([], 0, 0)
   return results;
 };
