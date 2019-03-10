0a2908d20307320f9c40c0a21ee1c4fe2e11779b
parkermuir <parker.muir@gmail.com>
2019-03-10 18:27:20 -0400

    linted

--- combinationSumII.js
+++ combinationSumII.js
@@ -1,30 +1,30 @@
 //# 40
-var combinationSum2 = function(candidates, target) {
+var combinationSum2 = function (candidates, target) {
   let results = [];
-  
+
   candidates = candidates.sort((a, b) => {
-      return a - b;
-  })
-  
-  console.log(candidates)
-  
+    return a - b;
+  });
+
+  console.log(candidates);
+
   let createCombinations = (currentCombo, sum, start) => {
-      if (sum === target) {
-          results.push(currentCombo);
-          return;
+    if (sum === target) {
+      results.push(currentCombo);
+      return;
+    }
+
+    for (let i = start; i < candidates.length; i++) {
+      let num = candidates[i];
+
+      if (i !== start && num === candidates[i - 1]) { continue; }
+
+      if (num + sum <= target) {
+        createCombinations(currentCombo.concat(num), sum + num, i + 1);
       }
-      
-      for (let i = start; i < candidates.length; i++) {
-          let num = candidates[i];
-          
-          if (i !== start && num === candidates[i - 1]) { continue; }
-          
-          if (num + sum <= target) {
-              createCombinations(currentCombo.concat(num), sum + num, i + 1);
-          }  
-      }
-  }
-  
-  createCombinations([], 0, 0)
+    }
+  };
+
+  createCombinations([], 0, 0);
   return results;
 };
