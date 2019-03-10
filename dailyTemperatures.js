a1175b67bce85175a16cf073d1b16309d19ec144
parkermuir <parker.muir@gmail.com>
2019-03-09 20:49:54 -0500

    daily temperatures solution

--- dailyTemperatures.js
+++ dailyTemperatures.js
@@ -4,14 +4,19 @@
  * @pram {number[]} T
  * @return {number[]}
  */
-var dailyTemperatures = function(T) {
-    
+var dailyTemperatures = function (T) {
+
   let results = new Array(T.length).fill(0);
-  let stack = [];
-  
-  for (let i = 0; i < T.length; i++) {
-      
+  let stack = [[0, T[0]]];
+
+  for (let i = 1; i < T.length; i++) {
+    let temperature = T[i];
+    while (stack.length !== 0 && temperature > stack[stack.length - 1][1]) {
+      let current = stack.pop();
+      results[current[0]] = i - current[0];
+    }
+    stack.push([i, temperature]);
   }
-  
-  
+
+  return results;
 };
