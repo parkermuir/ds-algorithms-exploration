19e1a7409ec10e9b2dc4ac3f590e79080aea7b12
parkermuir <parker.muir@gmail.com>
2019-03-09 21:27:26 -0500

    longest valid parens solution w/ stack

--- longestValidParens.js
+++ longestValidParens.js
@@ -5,6 +5,23 @@
  * @return {number}
  */
 var longestValidParentheses = function(s) {
+  let stack = [-1];
+  let max = 0;
+  
+  for (let i = 0; i < s.length; i++) {
+      //console.log({i, stack})
+      if (s.charAt(i) === ')') {
+          stack.pop()
+          if (stack.length === 0) {
+            stack.push(i)
+          }
+          max = Math.max(max, i - stack[stack.length - 1])
+      } else {
+          stack.push(i)
+      }
+      
+  }
 
+  return max;  
 
 };
