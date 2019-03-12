56051221ea05245de6dc7a661bfd073a886bcd40
parkermuir <parker.muir@gmail.com>
2019-03-12 12:05:33 -0400

    word break BFS solution

--- wordBreak.js
+++ wordBreak.js
@@ -5,6 +5,23 @@
  * @param {string[]} wordDict
  * @return {boolean}
  */
-var wordBreak = function(string, wordDict) {
+var wordBreak = function (string, wordDict) {
+  let wordSet = new Set(wordDict);
+  let q = [0];
 
+  while (q.length > 0) {
+    let start = q.shift();
+    for (let i = start; i < string.length; i++) {
+      let temp = string.substring(start, i + 1)
+      if (wordSet.has(temp)) {
+        if (i + 1 >= string.length) {
+          return true;
+        } else {
+          q.push(i + 1);
+        }
+      }
+    }
+  }
+
+  return false;
 };
