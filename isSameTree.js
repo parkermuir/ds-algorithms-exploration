e6be8fc5ab99f487c90ab59ff8869a3a9ee9db0f
parkermuir <parker.muir@gmail.com>
2019-03-13 19:02:03 -0400

    is same tree recursive solution

--- isSameTree.js
+++ isSameTree.js
@@ -20,3 +20,15 @@ var isSameTree = function(p, q) {
   }
   return stack1.length === stack2.length;
 };
+
+const isSameTreeRecursive = (p, q) => {
+  if (!p && !q) {
+    return true;
+  } else if (!p || !q) {
+    return false;
+  } else if (p.val !== q.val) {
+    return false;
+  } else {
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+}
