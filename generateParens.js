8064f7de38694c67ad71fcff110bb60e4e80b9fc
parkermuir <parker.muir@gmail.com>
2019-03-28 17:14:00 -0400

    generate parens backtrack solution

--- generateParens.js
+++ generateParens.js
@@ -3,5 +3,21 @@
  * @return {string[]}
  */
 var generateParenthesis = function (n) {
+  let results = [];
 
+  const build = (current, open, close) => {
+    if (current.length === 2 * n) {
+      results.push(current);
+      return;
+    }
+    if (open < n) {
+      build(current + '(', open + 1, close);
+    }
+    if (close < open) {
+      build(current + ')', open, close + 1);
+    }
+  };
+
+  build('', 0, 0);
+  return results;
 };
