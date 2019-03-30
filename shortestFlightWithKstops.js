44506e333da2ad0bacf7da1ac1a6c4029da62662
parkermuir <parker.muir@gmail.com>
2019-03-30 15:39:49 -0400

    shortest flight path solution (without real heap)

--- shortestFlightWithKstops.js
+++ shortestFlightWithKstops.js
@@ -1,4 +1,5 @@
-/**
+// # 787
+/*
  * @param {number} n
  * @param {number[][]} flights
  * @param {number} src
@@ -6,21 +7,46 @@
  * @param {number} K
  * @return {number}
  */
-var findCheapestPrice = function(n, flights, src, dst, K) {
-  const graph = createGraph(flights);
 
+var findCheapestPrice = function (n, flights, src, dst, K) {
+  const graph = createGraph(flights);
+  let q = [[src, 0, K + 1]];
+
+  while (q.length > 0) {
+    let [city, pathCost, stops] = q.shift();
+
+    if (city === dst) {
+      return pathCost;
+    } else {
+      if (stops > 0) {
+        if (graph.hasOwnProperty(city)) {
+          graph[city].forEach((dest) => {
+            //push to q next city, updated path cost, stops remaining
+            q.push([dest[0], pathCost + dest[1], stops - 1])
+          });
+        }
+
+        q.sort((a, b) => {
+          return a[1] - b[1];
+        });
+      }
+    }
+  }
+
+  return -1;
+  //return cost or -1 if route doesn't exist
 };
 
 const createGraph = (flights) => {
   let graph = {};
-  
+
   flights.forEach((flight) => {
-      let [start, dest, price] = flight;
-      if (graph.hasOwnProperty(start)) {
-         graph[start].push([dest, price]);
-      } else {
-          graph[start] = [[dest, price]];
-      }
+    let [start, dest, price] = flight;
+    if (graph.hasOwnProperty(start)) {
+      graph[start].push([dest, price]);
+    } else {
+      graph[start] = [[dest, price]];
+    }
   });
   return graph;
-}
+};
