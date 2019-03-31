91b4ffda94c3f99369af9f744578351a13960804
parkermuir <parker.muir@gmail.com>
2019-03-31 19:12:23 -0400

    non overlapping (merge intervals) solution, slow

--- nonOverlappingCharacterSubarrays.js
+++ nonOverlappingCharacterSubarrays.js
@@ -2,6 +2,7 @@
 
 const createArrays = (chars) => {
   let indexes = createIndexes(chars);
+  //console.log(indexes);
 
   let intervals = Object.values(indexes);
 
@@ -17,6 +18,8 @@ const createArrays = (chars) => {
     return a[0] - b[0];
   });
 
+  //console.log(intervals)
+
   const merged = [ intervals[0] ];
 
   for (let i = 1; i < intervals.length; i++) {
@@ -24,12 +27,17 @@ const createArrays = (chars) => {
     let prev = merged[merged.length - 1];
     
     if (interval[0] < prev[1]) {
-      merged[merged.length - 1] = [interval[0], prev[1]];
+      if (prev[1] > interval[1]) {
+        merged[merged.length - 1][1] = prev[1];
+      } else {
+        merged[merged.length - 1][1] = interval[1];
+      }
     } else {
       merged.push(interval);
     }
   }
 
+  //console.log(merged);
   return merged.map((el) => {
     return el[1] - el[0] + 1;
   });
@@ -44,10 +52,24 @@ const createIndexes = (chars) => {
       hash[char] = [i];
     }
   });
+  return hash;
 };
 
 
+console.log(createArrays(['A', 'B', 'C', 'A']));
+console.log(createArrays(['A', 'B', 'C', 'A', 'E', 'F']));
+console.log(createArrays(['A', 'B', 'C', 'D', 'A', 'B', 'C', 'D', 'B']));
 console.log(createArrays(['A', 'B', 'C', 'D']));
-console.log(createArrays(['A', 'B', 'C', 'D']));
-console.log(createArrays(['A', 'B', 'C', 'D']));
-console.log(createArrays(['A', 'B', 'C', 'D']));
+
+/*
+
+Create an object of every letter, and it's last index in the array, use that to slice
+
+OR
+
+Create an array of same size as inputs with the maximum last most occurrence up to that point, then dfs through that array, 
+if the last most index is equal to the current index, that position is a cut
+
+
+
+*/
