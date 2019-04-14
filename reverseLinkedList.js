a38d3ac851b4fd838558ff1394fd520d43391495
parkermuir <parker.muir@gmail.com>
2019-04-13 21:59:45 -0400

    reverse linked list solution formatted

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -14,15 +14,14 @@
 var reverseList = function(head) {
   let prev = null;
   let curr = head;
-  
-  
+
   while (curr) {
-      let temp = curr.next;
-      curr.next = prev;
-      
-      prev = curr;
-      curr = temp;
+    let temp = curr.next;
+    curr.next = prev;
+
+    prev = curr;
+    curr = temp;
   }
-  
+
   return prev;
 };
